a366366e2439be1924c3291cf36413c010e463ab
Jonathan Erthal <jonierthal@gmail.com>
2022-10-05 12:49:57 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,14 @@ import { Post } from "./Post"
 export function App() {
   return (
   <div> 
-   <Post/>
-   <Post/>
-   <Post/>
-   <Post/>
+   <Post
+      author="Jonahan Erthal"
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas at earum debitis cumque magni a nobis neque minima, maiores, beatae obcaecati tenetur natus repellat impedit eos. Adipisci esse nam saepe"
+   />
+   <Post
+      author="Marcos Almeida"
+      content="Novo Post"
+   />
   </div>
   )
 }
-
