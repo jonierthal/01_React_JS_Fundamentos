36e8653e37de510735bcb1692c6013e1d785b79b
Jonathan Erthal <jonierthal@gmail.com>
2022-10-07 13:02:54 -0300

    Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post"
 
+import styles from './App.module.css';
 import './global.css';
+
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
   <div> 
    <Header/>
 
-   <Post
-      author="Jonahan Erthal"
-      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas at earum debitis cumque magni a nobis neque minima, maiores, beatae obcaecati tenetur natus repellat impedit eos. Adipisci esse nam saepe"
-   />
-   <Post
-      author="Marcos Almeida"
-      content="Novo Post"
-   />
+   <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+         <Post
+            author="Jonahan Erthal"
+            content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas at earum debitis cumque magni a nobis neque minima, maiores, beatae obcaecati tenetur natus repellat impedit eos. Adipisci esse nam saepe"
+         />
+         <Post
+            author="Marcos Almeida"
+            content="Novo Post"
+         />
+      </main>
+   </div>
   </div>
   )
 }
