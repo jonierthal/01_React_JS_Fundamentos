88840d349876682dcedd699b532a863bf21567b1
Jonathan Erthal <jonierthal@gmail.com>
2022-10-14 14:12:46 -0300

    Componente:Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,36 @@
+import { ThumbsUp } from 'phosphor-react';
+import { Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+
+export function Comment () {
+    return(
+        <div className={styles.comment}>
+        <img src="https://github.com/jonierthal.png"/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Jonathan Erthal</strong>
+                            <time title="11 de Outubro as 12:48" dateTime="2022-11-10 12:47:30">Cerca de 2h atrás</time>
+                        </div>
+
+                        <button title="Deletar comentário">
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! </p>
+                </div>
+
+                <footer>
+                   <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+
+        </div>
+    )
+}
